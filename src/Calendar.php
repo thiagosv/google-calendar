1ff79c7f15847102c78cd8de2318c45451880367
Thiago <thiagosilvavieira97@gmail.com>
2018-02-15 16:38:47 -0200

    Correcao bugs.

--- src/Calendar.php
+++ src/Calendar.php
@@ -8,31 +8,33 @@
 
 namespace ThiagoSV\GoogleCalendar;
 
-class Calendar
-{
+class Calendar {
     private $client;
     private $trigger;
     private $event;
     private $params;
     private $service;
     private $colors;
-
+    
     /**
      * Calendar constructor.
      * Método responsável por inicializar a comunicação com a API do Google
      * @throws \Google_Exception
      */
-    public function __construct($ajax = NULL)
-    {
+    public function __construct($ajax = NULL, $path_user = NULL) {
+        
+        if ($path_user) {
+            $path_user = '/' . $path_user;
+        }
+        if ($ajax) {
+            $ajax = '../';
+        }
+        
         if (!defined('APPLICATION_NAME')) {
             define('APPLICATION_NAME', 'MLBIDDING');
         }
         if (!defined('CREDENTIALS_PATH')) {
-            if (is_dir("~")) {
-                define('CREDENTIALS_PATH', '~/.credentials/google-calendar.json');
-            } else {
-                define('CREDENTIALS_PATH', 'C:/Users/Thiago/.credentials/google-calendar.json');
-            }
+            define('CREDENTIALS_PATH', "~/.credentials{$path_user}/google-calendar.json");
         }
         if (!defined('CLIENT_SECRET_PATH')) {
             if ($ajax) {
@@ -44,76 +46,73 @@ class Calendar
         if (!defined('SCOPES')) {
             define('SCOPES', implode(' ', [\Google_Service_Calendar::CALENDAR]));
         }
-
+        
         $this->client = new \Google_Client();
         $this->client->setApplicationName(APPLICATION_NAME);
         $this->client->setScopes(SCOPES);
         $this->client->setAuthConfig(CLIENT_SECRET_PATH);
         $this->client->setAccessType('offline');
     }
-
+    
     /**
      * <b>getTrigger:</b> Método responsável por retornar mensagem de erro
      * @return mixed
      */
-    public function getTrigger()
-    {
+    public function getTrigger() {
         return $this->trigger;
     }
-
+    
     /**
      * <b>createClient:</b> Método responsável por gerar o link de autenticação da API
      * @return string
      */
-    public function createClient()
-    {
+    public function createClient() {
         $authUrl = $this->client->createAuthUrl();
         return $authUrl;
     }
-
+    
     /**
      * <b>setAccessToken:</b> Método responsável por criar o credenciamento da API,
      * salvar o arquivo .json no diretório especificado da define
      * @param $authCode
      * @return bool
      */
-    public function setAccessToken($authCode)
-    {
+    public function setAccessToken($authCode) {
         $accessToken = $this->client->fetchAccessTokenWithAuthCode($authCode);
         // Retorna o caminho absoluto
         $credentialsPath = $this->expandHomeDirectory(CREDENTIALS_PATH);
         // Cria o diretório de forma recursiva para armazenar o .json
         if (!file_exists(dirname($credentialsPath))) {
-            mkdir(dirname($credentialsPath), 0700, true);
+            mkdir(dirname($credentialsPath), 0700, TRUE);
         }
         // Salva o credenciamento dentro da pasta
         file_put_contents($credentialsPath, json_encode($accessToken));
-        return true;
+        return TRUE;
     }
-
+    
     /**
      * <b>getClient:</b> Método responsável por obter o client do Google
      * @return bool|\Google_Client
      */
-    public function getClient()
-    {
+    public function getClient() {
         // Load previously authorized credentials from a file.
         $credentialsPath = $this->expandHomeDirectory(CREDENTIALS_PATH);
+        
         if (!file_exists($credentialsPath)) {
             $this->trigger = 'Não há credenciais definidas!';
-            return false;
+            return FALSE;
         } else {
-            $accessToken = json_decode(file_get_contents($credentialsPath), true);
+            $accessToken = json_decode(file_get_contents($credentialsPath), TRUE);
             $this->client->setAccessToken($accessToken);
         }
-
+        
         if ($this->client->isAccessTokenExpired()) {
             if (is_file($credentialsPath)) {
                 unlink($credentialsPath);
-                return false;
+                return FALSE;
             }
         }
-
+        
         // Refresh the token if it's expired. /* nao esta vindo o refreshToken, entao essa parte do codigo ira ficar comentada*/
 //        if ($this->client->isAccessTokenExpired()) {
 //            $this->client->fetchAccessTokenWithRefreshToken($this->client->getRefreshToken());
@@ -121,93 +120,65 @@ class Calendar
 //        }
         return $this->client;
     }
-
+    
     /**
      * <b>createEvent:</b> Método responsável por criar o evento dentro do Google Calendar
-     * @param STRING $summary = Título do Evento
-     * @param STRING $location = Endereço completo do onde ocorrerá o evento
-     * @param STRING $description = Descrição do evento
-     * @param DATETIME $start = Data e Hora no formato americano
-     * @param DATETIME $end = Data e Hora no formato americano
-     * @param null|STRING $attendees = E-mail do Convidado
+     * @param STRING      $summary     = Título do Evento
+     * @param STRING      $location    = Endereço completo do onde ocorrerá o evento
+     * @param STRING      $description = Descrição do evento
+     * @param DATETIME    $start       = Data e Hora no formato americano
+     * @param DATETIME    $end         = Data e Hora no formato americano
+     * @param null|STRING $attendees   = E-mail do Convidado
      * @return bool|\Google_Service_Calendar_Event
      */
-    public function createEvent($summary, $location, $description, $start, $end, $attendees = null, $colorId = null)
-    {
+    public function createEvent($summary, $location, $description, $start, $end, $attendees = NULL, $colorId = NULL) {
         if (date('Y-m-d H:i:s', strtotime($start)) < date('Y-m-d H:i:s')) {
             $this->trigger = "A data inicial é menor do que a data atual, por favor verifique e tente novamente!";
-            return false;
+            return FALSE;
         }
         if (date('Y-m-d H:i:s', strtotime($end)) < date('Y-m-d H:i:s', strtotime($start))) {
             $this->trigger = "A data final é menor que a data de início, por favor verifique e tente novamente!";
-            return false;
+            return FALSE;
         }
-        $this->params = array(
-            'summary' => $summary,
-            'location' => $location,
-            'description' => $description,
-            'start' => array(
-                'dateTime' => date(DATE_ISO8601, strtotime($start)),
-                'timeZone' => 'America/Sao_Paulo',
-            ),
-            'end' => array(
-                'dateTime' => date(DATE_ISO8601, strtotime($end)),
-                'timeZone' => 'America/Sao_Paulo',
-            ),
-            'reminders' => array(
-                'useDefault' => false,
-                'overrides' => array(
-                    array('method' => 'email', 'minutes' => 24 * 60),
-                    array('method' => 'popup', 'minutes' => 10),
-                ),
-            ),
-        );
+        $this->params = array('summary' => $summary, 'location' => $location, 'description' => $description, 'start' => array('dateTime' => date(DATE_ISO8601, strtotime($start)), 'timeZone' => 'America/Sao_Paulo',), 'end' => array('dateTime' => date(DATE_ISO8601, strtotime($end)), 'timeZone' => 'America/Sao_Paulo',), 'reminders' => array('useDefault' => FALSE, 'overrides' => array(array('method' => 'email', 'minutes' => 24 * 60), array('method' => 'popup', 'minutes' => 10),),),);
         if (!empty($colorId)) {
-            $this->params += [
-                'colorId' => $colorId
-            ];
+            $this->params += ['colorId' => $colorId];
         }
         if (!empty($attendees)) {
             $emails = [];
-            foreach ($attendees as $val) {
+            foreach($attendees as $val) {
                 if (filter_var($val, FILTER_VALIDATE_EMAIL)) {
-                    $emails[] = [
-                        'email' => $val
-                    ];
+                    $emails[] = ['email' => $val];
                 }
             }
             if (!empty($emails)) {
-                $this->params += [
-                    'attendees' => $emails
-                ];
+                $this->params += ['attendees' => $emails];
             }
         }
         $this->event = new \Google_Service_Calendar_Event($this->params);
         $this->service = new \Google_Service_Calendar($this->client);
-        $this->event = $this->service->events->insert('primary', $this->event, ['sendNotifications' => true]);
+        $this->event = $this->service->events->insert('primary', $this->event, ['sendNotifications' => TRUE]);
         return $this->event;
     }
-
+    
     /**
      * <b>deleteEvent:</b> Método responsável por deletar um evento do Google Calendar
      * @param STRING $eventId = ID do evento do Google
      */
-    public function deleteEvent($eventId)
-    {
+    public function deleteEvent($eventId) {
         $this->service = new \Google_Service_Calendar($this->client);
-        $this->service->events->delete('primary', $eventId, ['sendNotifications' => true]);
+        $this->service->events->delete('primary', $eventId, ['sendNotifications' => TRUE]);
     }
-
+    
     /**
      * <b>getColors:</b> Método responsável por trazer as cores disponiveis no google calendar, para integracao com as cores.
      */
-    public function getColors($color_id = NULL)
-    {
+    public function getColors($color_id = NULL) {
         $this->service = new \Google_Service_Calendar($this->client);
         $this->colors = $this->service->colors->get();
-
+        
         if (!empty($color_id)) {
-            foreach ($this->colors->getEvent() as $key => $color) {
+            foreach($this->colors->getEvent() as $key => $color) {
                 if ($key == $color_id) {
                     return $color;
                 }
@@ -215,16 +186,15 @@ class Calendar
         } else {
             return $this->colors->getEvent();
         }
-
+        
     }
-
-    public function getListEvents()
-    {
+    
+    public function getListEvents() {
         $this->service = new \Google_Service_Calendar($this->client);
         $events = $this->service->events->listEvents('primary');
         $eventos = [];
-        while (true) {
-            foreach ($events->getItems() as $event) {
+        while(TRUE) {
+            foreach($events->getItems() as $event) {
                 $eventos[] = $event;
             }
             $pageToken = $events->getNextPageToken();
@@ -237,14 +207,13 @@ class Calendar
         }
         return $eventos;
     }
-
+    
     /**
      * <b>expandHomeDirectory:</b> Método responsável por expandir o diretório e normalizar o caminho absoluto
      * @param STRING $path = Caminho que deseja ser verificado
      * @return mixed
      */
-    private function expandHomeDirectory($path)
-    {
+    private function expandHomeDirectory($path) {
         $homeDirectory = getenv('HOME');
         if (empty($homeDirectory)) {
             $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
--- src/CalendarBD.php
+++ src/CalendarBD.php
@@ -10,44 +10,29 @@ namespace ThiagoSV\GoogleCalendar;
 
 use ThiagoSV\ControllerPDO;
 
-class CalendarBD
-{
-
+class CalendarBD {
+    
     private $trigger;
     private $query;
-    private $appointment_usuario_id;
-
-    public function __construct($appointment_user_id = NULL)
-    {
-        if(!empty($appointment_user_id)){
-            $this->appointment_user_id = appointment_user_id;
+    private $appointment_user_id;
+    
+    public function __construct($appointment_user_id = NULL) {
+        if (!empty($appointment_user_id)) {
+            $this->appointment_user_id = $appointment_user_id;
         }
     }
-
-    public function createEvent($event, $background = NULL)
-    {
-        $params = [
-            'appointment_title' => $event->getSummary(),
-            'appointment_description' => $event->getDescription(),
-            'appointment_location' => $event->getLocation(),
-            'appointment_event_id' => $event->getId(),
-            'appointment_start' => date('Y-m-d H:i:s', strtotime($event->getStart()->getDateTime())),
-            'appointment_end' => date('Y-m-d H:i:s', strtotime($event->getEnd()->getDateTime()))
-        ];
+    
+    public function createEvent($event, $background = NULL) {
+        $params = ['appointment_title' => $event->getSummary(), 'appointment_description' => $event->getDescription(), 'appointment_location' => $event->getLocation(), 'appointment_event_id' => $event->getId(), 'appointment_start' => date('Y-m-d H:i:s', strtotime($event->getStart()->getDateTime())), 'appointment_end' => date('Y-m-d H:i:s', strtotime($event->getEnd()->getDateTime()))];
         if (!empty($background) && is_array($background)) {
-            $params += [
-                'appointment_background' => $background[0],
-                'appointment_foreground' => $background[1]
-            ];
+            $params += ['appointment_background' => $background[0], 'appointment_foreground' => $background[1]];
         }
         if (!empty($this->appointment_user_id)) {
-            $params += [
-                'appointment_user_id' => $this->appointment_user_id
-            ];
+            $params += ['appointment_user_id' => $this->appointment_user_id];
         }
         $create = new ControllerPDO\Create;
         $create->create("appointment", $params);
-
+        
         if (empty($create->getResult())) {
             $this->trigger = ['error', 'Oops!', 'Erro ao cadastrar evento, se persistir entre em contato com o administrador.'];
         } else {
@@ -59,9 +44,8 @@ class CalendarBD
             }
         }
     }
-
-    public function deleteEvent($id)
-    {
+    
+    public function deleteEvent($id) {
         if (empty($this->getEvent($id))) {
             $this->trigger = ['error', 'Oops!', 'Evento não encontrado para delete.'];
         } else {
@@ -74,19 +58,17 @@ class CalendarBD
             }
         }
     }
-
-    public function getTrigger()
-    {
+    
+    public function getTrigger() {
         return $this->trigger;
     }
-
-    public function getEvent($appointment_id = NULL, $appointment_event_id = NULL, $limit = NULL)
-    {
+    
+    public function getEvent($appointment_id = NULL, $appointment_event_id = NULL, $limit = NULL) {
         $read = new ControllerPDO\Read;
         $this->query = new ControllerPDO\QueryCreator();
         $this->query->from("appointment");
         $this->query->select("*");
-
+        
         $dados = [];
         $i = 0;
         if (!empty($appointment_id)) {
@@ -97,9 +79,9 @@ class CalendarBD
             $this->query->where("appointment_event_id = :appointment_event_id");
             $this->query->places("appointment_event_id={$appointment_event_id}");
         }
-        if (!empty($this->appointment_usuario_id)) {
-            $this->query->where("appointment_usuario_id = :appointment_usuario_id");
-            $this->query->places("appointment_usuario_id={$this->appointment_usuario_id}");
+        if (!empty($this->appointment_user_id)) {
+            $this->query->where("appointment_user_id = :appointment_user_id");
+            $this->query->places("appointment_user_id={$this->appointment_user_id}");
         }
         $this->query->order(["appointment_start" => "ASC"]);
         if (!empty($limit)) {
@@ -108,7 +90,7 @@ class CalendarBD
         $read->readFull($this->query->getQuery(), $this->query->getPlaces());
         if ($read->getResult()) {
             $dados = $read->getResult();
-            foreach ($read->getResult() as $result) {
+            foreach($read->getResult() as $result) {
                 $dados[$i] = $result;
                 $read->readFull("SELECT * FROM attendees WHERE attendees_appointment_id = :attendees_appointment_id", "attendees_appointment_id={$result['appointment_id']}");
                 if ($read->getResult()) {
@@ -117,22 +99,17 @@ class CalendarBD
                 $i++;
             }
         }
-
+        
         return $dados;
     }
-
-    private function createAttendees($attendeesList, $appointmentId)
-    {
-        foreach ($attendeesList as $attendees) {
-            $params = [
-                'attendees_appointment_id' => $appointmentId,
-                'attendees_name' => $attendees->getDisplayName(),
-                'attendees_email' => $attendees->getEmail()
-            ];
-
+    
+    private function createAttendees($attendeesList, $appointmentId) {
+        foreach($attendeesList as $attendees) {
+            $params = ['attendees_appointment_id' => $appointmentId, 'attendees_name' => $attendees->getDisplayName(), 'attendees_email' => $attendees->getEmail()];
+            
             $create = new ControllerPDO\Create;
             $create->create("attendees", $params);
-
+            
             if (empty($create->getResult())) {
                 $this->trigger = ['error', 'Oops!', 'Erro ao cadastrar participantes do evento no banco de dados.'];
             }
